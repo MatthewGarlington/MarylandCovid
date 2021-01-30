36ceac8462723f67a11cb6d322f633a248bff375
Matthew Garlington <SBMatthewG@gmail.com>
2021-01-30 16:21:59 -0500

    Initial Commit

---
+++ MarlyandCovidData/MarlyandCovidDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarlyandCovidDataApp.swift
+//  MarlyandCovidData
+//
+//  Created by Matthew Garlington on 1/30/21.
+//
+
+import SwiftUI
+
+@main
+struct MarlyandCovidDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
