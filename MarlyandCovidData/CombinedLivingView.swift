ec8fc4917451c76dd465ba955445f009b451082f
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-04 10:03:05 -0500

    Feat: Added List of Combined living cases of Covid 19 in MD
    Added a view to display the combined living situations of nursing homes, assisted livings, and prisons in the state of maryland with data from MD gov to display staff and resident cases using a for in loop for the data to be shown

--- MarlyandCovidData/CombinedLivingView.swift
+++ MarlyandCovidData/CombinedLivingView.swift
@@ -8,8 +8,58 @@
 import SwiftUI
 
 struct CombinedLivingView: View {
+    
+    @ObservedObject var vm = TotalCongergateLiving()
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        NavigationView {
+        List {
+            ForEach(vm.recentLivingCases, id: \.self) { num in
+                
+                VStack(alignment: .leading, spacing: 20) {
+                    
+                    
+            
+                    
+                    HStack {
+                 
+                        Text(num.facility_name ?? "")
+                            .bold()
+                            
+                    }
+                    
+                        HStack {
+                            
+                            Text("County:")
+                                .bold()
+                            Text(num.county ?? "")
+                        }
+                    
+                    
+                    
+                    HStack {
+                        
+                        Text("Residents Cases:")
+                            .bold()
+                        Text(num.residents_private ?? "0")
+                    }
+                    
+                    HStack {
+                        
+                        Text("Staff Cases:")
+                            .bold()
+                        Text(num.staff_private ?? "0")
+                    }
+                
+                
+                
+                    
+                }
+                .font(.title3)
+                
+            }
+        }.navigationTitle("Combined Living Cases")
+        }
     }
 }
 
