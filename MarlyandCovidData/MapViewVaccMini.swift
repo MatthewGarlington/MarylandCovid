6b68140e033412e85fd8796cad6c06b6f2566c2a
Matthew Garlington <SBMatthewG@gmail.com>
2021-02-05 15:35:07 -0500

    Feat: Initial Commit

---
+++ MarlyandCovidData/MapViewVaccMini.swift
@@ -0,0 +1,20 @@
+//
+//  MapViewVaccMini.swift
+//  MarlyandCovidData
+//
+//  Created by Matthew Garlington on 2/5/21.
+//
+
+import SwiftUI
+
+struct MapViewVaccMini: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MapViewVaccMini_Previews: PreviewProvider {
+    static var previews: some View {
+        MapViewVaccMini()
+    }
+}
---
+++ MarlyandCovidData/VaccineLocationDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  VaccineLocationDetailView.swift
+//  MarlyandCovidData
+//
+//  Created by Matthew Garlington on 2/4/21.
+//
+
+import SwiftUI
+
+struct VaccineLocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct VaccineLocationDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        VaccineLocationDetailView()
+    }
+}
